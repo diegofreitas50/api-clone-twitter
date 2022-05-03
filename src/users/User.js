94428c8d36c590553502d1933690f59ea6a7ab69
diegofreitas50 <diegofreitas50@hotmail.com>
2022-05-02 22:19:23 -0300

    User model e documentos user

---
+++ src/users/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowecase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    avatar: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
